a7183822fecbf81127e99251ac47c8d2c9784969
Asad Moosvi <moosvi.asad@gmail.com>
2021-09-05 22:43:59 +0500

    Convert function constructor to Class

--- index.js
+++ index.js
@@ -40,28 +40,30 @@ async function getCoverUrl(title, author) {
     })
 }
 
-function Book(name, author, pages, read, description, coverUrl) {
-  // make sure arguments are provided
-  if (
-    name == undefined ||
-    author == undefined ||
-    pages == undefined ||
-    read == undefined ||
-    description == undefined
-  ) {
-    throw new Error(
-      'First 5 arguments are required: name, author, pages, read, and description'
-    );
-  }
-  this.name = capitalize(name);
-  this.author = capitalize(author);
-  this.pages = pages;
-  this.read = read;
-  this.description = description;
-  this.coverUrl = coverUrl;
-  if (!this.coverUrl) {
-    this.coverUrl =
-      'https://via.placeholder.com/400x600?text=Missing+Cover+Image';
+class Book {
+  constructor(name, author, pages, read, description, coverUrl) {
+    // make sure arguments are provided
+    if (
+      name == undefined ||
+      author == undefined ||
+      pages == undefined ||
+      read == undefined ||
+      description == undefined
+    ) {
+      throw new Error(
+        'First 5 arguments are required: name, author, pages, read, and description'
+      );
+    }
+    this.name = capitalize(name);
+    this.author = capitalize(author);
+    this.pages = pages;
+    this.read = read;
+    this.description = description;
+    this.coverUrl = coverUrl;
+    if (!this.coverUrl) {
+      this.coverUrl =
+        'https://via.placeholder.com/400x600?text=Missing+Cover+Image';
+    }
   }
 }
 
